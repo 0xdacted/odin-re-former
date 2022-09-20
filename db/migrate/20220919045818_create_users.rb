e96e0dd3bec0985107b8e95c85c94cc40f08703d
Red <crabmarket56@gmail.com>
2022-09-20 10:27:51 +0900

    Add validations

--- db/migrate/20220919045818_create_users.rb
+++ db/migrate/20220919045818_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username, presence: true
-      t.string :email, presence: true
-      t.string :password, presence: true
+      t.string :username
+      t.string :email
+      t.string :password
 
       t.timestamps
     end
