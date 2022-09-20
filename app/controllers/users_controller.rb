6e8c667abe431c4e83fc374253b0cee3d9b41c9f
Red <crabmarket56@gmail.com>
2022-09-20 10:20:21 +0900

    Draft edit logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   end
 
   def create
-
    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
    @user = User.new(user_params)
 
@@ -13,6 +12,20 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
   end
 
